a5b85377c1f86b1d24388269642141e384944692
Sheddy <shedrackitulah@gmail.com>
2023-04-29 14:01:25 +0100

    chore: :wrench: next config

--- client/next.config.js
+++ client/next.config.js
@@ -1,8 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  images: {
+    domains: [process.env.NEXT_PUBLIC_SERVER_DOMAIN],
+  },
   experimental: {
     appDir: true,
   },
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
