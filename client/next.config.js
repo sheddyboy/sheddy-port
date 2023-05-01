58c07adb0ec782acd2f830e6c1df4c24bd74a50e
Sheddy <shedrackitulah@gmail.com>
2023-05-01 03:21:26 +0100

    chore: :wrench: update nextconfig

--- client/next.config.js
+++ client/next.config.js
@@ -1,10 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: [
-      process.env.NEXT_PUBLIC_SERVER_DOMAIN,
-      "https://res.cloudinary.com",
-    ],
+    domains: ["res.cloudinary.com"],
   },
   experimental: {
     appDir: true,
